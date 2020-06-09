11a8d515a9079f93925120530da73904b8f6daf2
Erlinda Chambi Manzano <chambimanzanoerlinda@gmail.com>
2020-06-08 23:44:21 -0400

    first commit

---
+++ src/_nav.js
@@ -0,0 +1,30 @@
+export default {
+  items: [
+
+    {
+      title: true,
+      name: 'Agenda Virtual',
+    },
+    {
+      name: 'SunEditor',
+      url: '/wysiwyg/suneditor',
+      icon: 'icon-start',
+    },
+    {
+      name: 'TinyMCE',
+      url: '/wysiwyg/tinymce',
+      icon: 'icon-start',
+    },
+    {
+      name: 'React-Draf-Wysiwyg',
+      url: '/wysiwyg/draft',
+      icon: 'icon-start',
+    },
+    {
+      name: 'WEB-React-Draf-Wysiwyg',
+      url: '/wysiwyg/draftweb',
+      icon: 'icon-start',
+    },
+
+  ],
+};
